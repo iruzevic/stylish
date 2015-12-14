43a31af030a494c32998b338c24f06905d923793
Ivan Ruzevic <ivan.ruzevic@i-ways.hr>
2015-12-14 18:36:18 +0100

    Version 4

--- preview/parts/buttons.php
+++ preview/parts/buttons.php
@@ -8,36 +8,6 @@
             <!-- Button Black -->
             <a href="#" class="btn btn_mine_shaft">Button Black</a>
             &nbsp;
-            <!-- Button  -->
-            <a href="#" class="btn btn_havelock_blue">Button Blue</a>
-            &nbsp;
-            <!-- Button Red -->
-            <a href="#" class="btn btn_flush_mahogany">Button Red</a>
-            &nbsp;
-            <!-- Button Green -->
-            <a href="#" class="btn btn_emerald">Button Green</a>
-            &nbsp;
-            <!-- Button Yellow -->
-            <a href="#" class="btn btn_earls_green">Button Yellow</a>
-            &nbsp;
-            <!-- Button Purple -->
-            <a href="#" class="btn btn_amethyst">Button Purple</a>
-            &nbsp;
-            <!-- Button Orange -->
-            <a href="#" class="btn btn_raw_sienna">Button Orange</a>
-            &nbsp;
-            <!-- Button Gray -->
-            <a href="#" class="btn btn_dove_gray">Button Gray</a>
-            &nbsp;
-            <!-- Button Gray Light -->
-            <a href="#" class="btn btn_dove_gray_light">Button Gray Light</a>
-            &nbsp;
-            <!-- Button Gray Lighter -->
-            <a href="#" class="btn btn_silver">Button Gray Lighter</a>
-            &nbsp;
-            <!-- Button Light -->
-            <a href="#" class="btn btn_mercury">Button Light</a>
-            &nbsp;
             <!-- Button With No radius -->
             <a href="#" class="btn btn_mine_shaft no_radius">Button No Radius</a>
             <br/>
@@ -48,9 +18,6 @@
             <!-- Button Rounded -->
             <a href="#" class="btn btn_mine_shaft rounded">Button Rounded</a>
             &nbsp;
-            <!-- Button Disabled -->
-            <a href="#" class="btn btn_mine_shaft disabled">Button Disabled</a>
-            &nbsp;
             <!-- Button With Icon on the left -->
             <a href="#" class="btn btn_mine_shaft btn_icon_left"><i class="sy_info_round"></i>Button With Icon</a>
             &nbsp;
@@ -59,59 +26,27 @@
 
         </div>
         <div class="source">
-                            <pre class="brush: xml;">
+            <pre class="brush: xml;">
 
-                                <!-- Button Black -->
-                                <a href="#" class="btn btn_mine_shaft">Button Black</a>
-                                &nbsp;
-                                <!-- Button  -->
-                                <a href="#" class="btn btn_havelock_blue">Button Blue</a>
-                                &nbsp;
-                                <!-- Button Red -->
-                                <a href="#" class="btn btn_flush_mahogany">Button Red</a>
-                                &nbsp;
-                                <!-- Button Green -->
-                                <a href="#" class="btn btn_emerald">Button Green</a>
-                                &nbsp;
-                                <!-- Button Yellow -->
-                                <a href="#" class="btn btn_earls_green">Button Yellow</a>
-                                &nbsp;
-                                <!-- Button Purple -->
-                                <a href="#" class="btn btn_amethyst">Button Purple</a>
-                                &nbsp;
-                                <!-- Button Orange -->
-                                <a href="#" class="btn btn_raw_sienna">Button Orange</a>
-                                &nbsp;
-                                <!-- Button Gray -->
-                                <a href="#" class="btn btn_dove_gray">Button Gray</a>
-                                &nbsp;
-                                <!-- Button Gray Light -->
-                                <a href="#" class="btn btn_dove_gray_light">Button Gray Light</a>
-                                &nbsp;
-                                <!-- Button Gray Lighter -->
-                                <a href="#" class="btn btn_silver">Button Gray Lighter</a>
-                                &nbsp;
-                                <!-- Button Light -->
-                                <a href="#" class="btn btn_mercury">Button Light</a>
-                                &nbsp;
-                                <!-- Button With No radius -->
-                                <a href="#" class="btn btn_mine_shaft no_radius">Button No Radius</a>
-                                &nbsp;
-                                <!-- Button Auto width -->
-                                <a href="#" class="btn btn_mine_shaft auto_width">Button Auto width</a>
-                                &nbsp;
-                                <!-- Button Rounded -->
-                                <a href="#" class="btn btn_mine_shaft rounded">Button Rounded</a>
-                                &nbsp;
-                                <!-- Button Disabled -->
-                                <a href="#" class="btn btn_mine_shaft disabled">Button Disabled</a>
-                                &nbsp;
-                                <!-- Button With Icon on the left -->
-                                <a href="#" class="btn btn_mine_shaft btn_icon_left"><i class="fa fa-life-ring"></i>Button With Icon</a>
-                                &nbsp;
-                                <!-- Button With Icon on the right -->
-                                <a href="#" class="btn btn_mine_shaft btn_icon_right">Button With Icon<i class="fa fa-life-ring"></i></a>
-                            </pre>
+                 <!-- Button Black -->
+                <a href="#" class="btn btn_mine_shaft">Button Black</a>
+                &nbsp;
+                <!-- Button With No radius -->
+                <a href="#" class="btn btn_mine_shaft no_radius">Button No Radius</a>
+                <br/>
+                <!-- Button Auto width -->
+                <a href="#" class="btn btn_mine_shaft auto_width">Button Auto width</a>
+                &nbsp;
+                <br/><br/>
+                <!-- Button Rounded -->
+                <a href="#" class="btn btn_mine_shaft rounded">Button Rounded</a>
+                &nbsp;
+                <!-- Button With Icon on the left -->
+                <a href="#" class="btn btn_mine_shaft btn_icon_left"><i class="sy_info_round"></i>Button With Icon</a>
+                &nbsp;
+                <!-- Button With Icon on the right -->
+                <a href="#" class="btn btn_mine_shaft btn_icon_right">Button With Icon<i class="sy_info_round"></i></a>
+            </pre>
         </div>
     </div>
 </div>
