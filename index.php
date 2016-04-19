c507992049d521ce754aea12e44301f19bc86ed7
Ivan Ruzevic <ivan.ruzevic@i-ways.hr>
2016-04-19 19:07:57 +0200

    Fixing wiki

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('wiki/content/head.php') ?>
+<?php include('wiki/content/header.php') ?>
+
+
+
+
+<?php include('wiki/content/footer.php') ?>
--- wiki/index.php
+++
@@ -1,7 +0,0 @@
-<?php include('content/head.php') ?>
-<?php include('content/header.php') ?>
-
-
-
-
-<?php include('content/footer.php') ?>
